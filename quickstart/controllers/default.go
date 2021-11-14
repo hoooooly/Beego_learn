494c9e193b44ee8bea79fb491d353bc9589b9f2e
hoooooly <espchenhao@qq.com>
2021-11-14 20:23:02 +0800

    beego

--- quickstart/controllers/default.go
+++ quickstart/controllers/default.go
@@ -15,11 +15,11 @@ type UserController struct {
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+	c.TplName = "index.html"
 }
 
 func (c *UserController) Get() {
 	c.Data["Website"] = "UserController.web"
 	c.Data["Email"] = "espholychan@outlook.com"
-	c.TplName = "index.tpl"
+	c.TplName = "index.html"
 }
